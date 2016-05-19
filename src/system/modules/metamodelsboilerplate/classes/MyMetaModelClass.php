14526cf99d8ae0a2fac76dc428ae7cf3f745e00e
Ingolf Steinhardt <info@e-spin.de>
2016-05-19 18:00:58 +0200

    add event_listeners and autload templates

---
+++ src/system/modules/metamodelsboilerplate/classes/MyMetaModelClass.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of MetaModels/boilerplate.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Boilerplate
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/boilerplate/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/**
+ * you can use all Contao hooks
+ * look at https://docs.contao.org/books/api/extensions/hooks/index.html
+ * and activate the hook at config.php
+ * and autoload the class at autoload.php
+ */
+/*
+class MyMetaModelClass
+{
+    public function myMetaModelFunction($param)
+    {
+        // function
+    }
+}
+*/
---
+++ src/system/modules/metamodelsboilerplate/config/event_listeners.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of MetaModels/boilerplate.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Boilerplate
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/boilerplate/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/**
+ * EVENTS
+ *
+ * change the placeholder <...> and use the
+ * template with or without priority
+ */
+
+/*
+use ContaoCommunityAlliance\DcGeneral\<Path>\<Event-Class>;
+
+// without priority
+return array(
+    <Event-Class>::NAME => array(
+        function (<Event-Class> $event) {
+            // function...
+        }
+    )
+);
+
+// with priority -1
+return array(
+    <Event-Class>::NAME => array(
+        array(
+            function (<Event-Class> $event) {
+                // function...
+            },
+            -1
+        }
+    )
+);
+*/
